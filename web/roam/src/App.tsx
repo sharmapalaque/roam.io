35f6f98ab35888fcd884edcad2c904d73fb1dd08
sanket1305 <meetsanket24@gmail.com>
2025-02-09 00:06:20 -0500

    Trying out Layout library for listview

--- web/roam/src/App.tsx
+++ web/roam/src/App.tsx
@@ -1,11 +1,26 @@
-import './App.css'
-import Register from './components/Register/Register'
+import "./App.css";
+import Register from "./components/Register/Register";
+import { Layout, Typography } from "antd";
+const { Header, Content } = Layout
+const {Title} = Typography
 
 function App() {
-
   return (
-    <Register />
-  )
+    <>
+      <Register />
+      <Layout>
+        <Header className="header">
+          <Title
+            level={3}
+            style={{ color: "white", textAlign: "center", marginTop: "15px" }}
+          >
+            React Demo
+          </Title>
+        </Header>
+        <Layout style={{padding:""}}></Layout>
+      </Layout>
+    </>
+  );
 }
 
-export default App
+export default App;
