00d128d9297e598aa06e9a2a853dd482114fbc08
Pragyna-Abhishek <pragynaabhishek2013@gmail.com>
2025-03-01 11:52:56 -0500

    login messages are now json and changed user age to dob

--- back_end/models/user.go
+++ back_end/models/user.go
@@ -1,11 +1,13 @@
 package models
 
+import "time" // Added time import for the date type
+
 // User represents a user object in the system
 type User struct {
-	ID       uint   `gorm:"primaryKey"`
-	Name     string `gorm:"size:100"`
-	Username string `gorm:"size:50;uniqueIndex"` // unique username
-	Email    string `gorm:"uniqueIndex"`
-	Age      int    `gorm:"not null"` // age cannot be null
+	ID       uint      `gorm:"primaryKey"`
+	Name     string    `gorm:"size:100"`
+	Username string    `gorm:"size:50;uniqueIndex"` // unique username
+	Email    string    `gorm:"uniqueIndex"`
+	Dob      time.Time `gorm:"not null"` // date of birth cannot be null
 	Password string
 }
