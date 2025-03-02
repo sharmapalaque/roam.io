c70183f9fcee3cd6f9637dae53433891e72f51d8
sanket1305 <meetsanket24@gmail.com>
2025-03-02 16:49:35 -0500

    corrected route.go file

--- back_end/routes/routes.go
+++ back_end/routes/routes.go
@@ -11,7 +11,7 @@ import (
 	"gorm.io/gorm" // This is needed to serve Swagger UI
 )
 
-func NewRouter(db *gorm.DB) {
+func NewRouter(db *gorm.DB) *mux.Router {
 	r := mux.NewRouter()
 
 	// Define user-related routes
@@ -46,5 +46,5 @@ func NewRouter(db *gorm.DB) {
 	// Start the server
 	fmt.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(originsOk, headersOk, methodsOk)(r)))
-	// return r;
+	return r;
 }
