4389551dfb93aa3acf435511a06278382f597936
shaurya <sshaurya84@yahoo.com>
2025-03-29 18:18:22 -0400

    add required fields to accomodation table

--- back_end/models/accommodation.go
+++ back_end/models/accommodation.go
@@ -11,4 +11,6 @@ type Accommodation struct {
 	Facilities  pq.StringArray `gorm:"type:text[]"`
 	ImageUrls   pq.StringArray `gorm:"type:text[]"`
 	UserReviews pq.StringArray `gorm:"type:text[]"`
+	HostID      uint           `gorm:"not null"`
+	Guests      uint           `gorm:"not null"`
 }
