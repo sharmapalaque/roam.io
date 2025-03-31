0be586d0729aa62dab0301db41dcc7bc0f78e4aa
shaurya <sshaurya84@yahoo.com>
2025-03-30 21:55:56 -0400

    add events table

--- back_end/db/connection.go
+++ back_end/db/connection.go
@@ -21,7 +21,7 @@ func Connect() (*gorm.DB, error) {
 }
 
 func MigrateDB(db *gorm.DB) {
-	err := db.AutoMigrate(&models.User{}, &models.Accommodation{}, &models.Booking{}, models.Host{})
+	err := db.AutoMigrate(&models.User{}, &models.Accommodation{}, &models.Booking{}, models.Host{}, models.Event{}, models.Organizer{})
 	if err != nil {
 		panic("Failed to migrate database")
 	}
