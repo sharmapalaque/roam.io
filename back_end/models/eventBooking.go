d1083c7535140ef8dc54983a7059b2ef8d0e4216
shaurya <sshaurya84@yahoo.com>
2025-03-31 00:33:13 -0400

    add event booking api for users to register for events

--- back_end/models/eventBooking.go
+++ back_end/models/eventBooking.go
@@ -1,14 +1,10 @@
 package models
 
-import "time"
-
 // User represents a user object in the system
 type EventBooking struct {
-	ID           uint `gorm:"primaryKey"`
-	UserID       uint `gorm:"not null"`
-	EventId      uint `gorm:"not null"`
-	CheckinDate  time.Time
-	CheckoutDate time.Time
-	Guests       uint `gorm:"not null"`
-	TotalCost    uint `gorm:"not null"`
+	ID        uint `gorm:"primaryKey"`
+	UserID    uint `gorm:"not null"`
+	EventId   uint `gorm:"not null"`
+	Guests    uint `gorm:"not null"`
+	TotalCost uint `gorm:"not null"`
 }
