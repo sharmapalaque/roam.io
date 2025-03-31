04c52fde053aad78769d295aa700f184def82420
sanket1305 <meetsanket24@gmail.com>
2025-03-31 16:30:23 -0400

    accomodation details comments removed

--- web/roam/src/components/ListView/AccommodationDetails.tsx
+++ web/roam/src/components/ListView/AccommodationDetails.tsx
@@ -255,12 +255,6 @@ const AccommodationDetails: React.FC = () => {
           // console.log("lets' see response");
           // console.log(result);
 
-          // //setting the missing fields
-          // result.PricePerNight = accommodation.PricePerNight;
-          // result.Rating = accommodation.Rating;
-          // result.UserReviews = accommodation.UserReviews;
-          // result.Owner = accommodation.Owner;
-
           setAccommodation(result)
 
         } catch (error) {
